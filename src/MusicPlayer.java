14d88f40f5b215e2b2f4068e0c580d1fa83fd415
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-18 15:34:43 -0600

    music playable

---
+++ src/MusicPlayer.java
@@ -0,0 +1,21 @@
+public class MusicPlayer extends  Player{
+    private String voice;
+
+    public MusicPlayer(String voice) {
+        this.voice = voice;
+    }
+
+    public MusicPlayer() {
+        voice = "";
+    }
+
+    @Override
+    public void play(Album album) {
+
+    }
+
+    @Override
+    public void play(Song song) {
+
+    }
+}
