b08313dd58eb7c389e984daca998ac7a96a0d729
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-18 15:51:17 -0600

    exceptions added

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 public class MusicPlayer extends  Player{
     private String voice;
 
@@ -16,6 +18,22 @@ public class MusicPlayer extends  Player{
 
     @Override
     public void play(Song song) {
-
+        Runtime r = Runtime.getRuntime();
+        try {
+            Thread.sleep(MusicPlayer.INTRO_PAUSE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for(String lines: song.getLyrics()){
+           for(String words: Song.parseLyrics(lines)){
+               try {
+                   r.exec("say "+words);
+                   Thread.sleep(MusicPlayer.WORD_CADENCE);
+               } catch (IOException | InterruptedException e) {
+                   e.printStackTrace();
+               }
+           }
+        }
     }
+
 }
