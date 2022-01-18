61c18a268db50251f9094e714640d04079e4b444
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-18 12:17:38 -0600

    karaoke project

---
+++ src/Song.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class Song {
+
+    private String title;
+    private String artist;
+    private List<String> lyrics;
+
+    public Song(String title, String artist, List<String> lyrics) {
+        this.title = title;
+        this.artist = artist;
+        this.lyrics = lyrics;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public List<String> getLyrics() {
+        return lyrics;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setLyrics(List<String> lyrics) {
+        this.lyrics = lyrics;
+    }
+}
